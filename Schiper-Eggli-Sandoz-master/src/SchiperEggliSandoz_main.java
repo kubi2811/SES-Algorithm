1ba37933061a1de11f5779baee0bd37d85fbb136
kuppi.le@alpaca.vn <hieutb308@gmail.com>
2023-03-11 12:15:27 +0700

    rebuild source

--- Schiper-Eggli-Sandoz-master/src/SchiperEggliSandoz_main.java
+++ Schiper-Eggli-Sandoz-master/src/SchiperEggliSandoz_main.java
@@ -10,15 +10,15 @@ public class SchiperEggliSandoz_main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int numProcesses = 4;
+		int numProcesses = 3;
 		Thread[] myThreads = new Thread[numProcesses];
 		try{
 			// Create Registry
 			Registry registry = LocateRegistry.createRegistry(1099);
 
-			int[][] destIDs = {{1,2,3,4,5,6}, {0,2,3,4,5,6}, {1}, {2,0}};
-			String[][] messages = {{"1", "2"}, {}, {"3"}, {"4","giaminh"}};
-			int[][] delays = {{5000, 0}, {}, {500}, {300,200}};
+			int[][] destIDs = {{1,2}, {}, {1}};
+			String[][] messages = {{"1", "2"}, {}, {"3"}};
+			int[][] delays = {{5000, 0}, {}, {500}};
 
 			for (int i = 0; i < numProcesses; i++)
 			{
